f4e2eca33fa92517ec9d834241c9f6337359fbe9
obed325 <obed325@hotmail.com>
2020-12-10 13:19:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FindNumbersInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindNumbersInString();
+        }
+        static void FindNumbersInString()
+        {
+            string sentence = "10 cats, 20 dogs, 40 fish and 1 programmer.";
+
+            string w;
+            int c = 0;
+            string[] digits = System.Text.RegularExpressions.Regex.Split(sentence, @"\D+");
+            foreach (string item in digits)
+            {
+                int number;
+                if (int.TryParse(item, out number))
+                {
+                    Console.WriteLine(number);
+                }
+            }
+
+        }
+    }
+}
